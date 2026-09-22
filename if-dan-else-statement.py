fac0c7187e82878d550c64cbbef10e1daa8837fe
Namirah <namirahalbanjary@gmail.com>
2026-09-22 23:40:52 +0700

    Add files via upload

---
+++ if-dan-else-statement.py
@@ -0,0 +1,29 @@
+# If dan Else Statement
+
+# 1. If nya
+# 2. kondisinya
+# 3. aksinya
+
+nama = input("Siapa nama Anda? ")
+
+# program sebelumnya
+# if kondisi aksi
+# program selanjutnya
+
+# 1 program if inline
+if nama == "namirah" : print("Kamu orang hebat")
+print(f"Terima kasih {nama}")
+
+# 2. program if indentation
+
+if nama == "namirah":
+         print("kamu orang hebat")
+         print("kamu juga orang hebat")
+print(f"Terima kasih {nama}")
+
+# 3. Else statement
+if nama=="albanjary":
+        print("hai albanjary, orang paling hebat")
+else:
+        print("kamu bukan albanjary, kamu bukan orang hebat")
+print("akhir dari program")
