2d2baa67a605f2bb4806d9a8d565766c5abe678d
Namirah <namirahalbanjary@gmail.com>
2026-09-23 16:38:51 +0700

    Add files via upload

---
+++ elif-statement.py
@@ -0,0 +1,14 @@
+# ELIF = else if statement
+
+nama = input("Nama kamu siapa? : ")
+
+if nama == "namirah": # kondisi 1
+    print("Hai cantik, selamat datang!") # aksi true 1
+elif nama == "bana": # kondisi ke 2
+    print("hai bana jelek")
+elif nama == "nisa":
+    print ("hai nisa jelek")
+else:
+    print("ah kamu siapa")
+
+print("akhir dari program")
